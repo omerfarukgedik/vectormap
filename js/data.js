adb2efabdfe4a7061e383a633408f291424e9d89
Ömer Gedik <omerfarukgedikk@gmail.com>
2020-03-24 13:59:42 +0300

    first commit

---
+++ js/data.js
@@ -0,0 +1,38 @@
+const cities = {
+  "01": {
+    "title" : "ADANA İLİ MADEN VE ENERJİ KAYNAKLARI",
+    "img" : "./images/adana.jpg",
+    "content" : "Adana ili Türkiye’nin güneyinde Akdeniz Bölgesi’nin Doğu Akdeniz Bölümü’nde yer alan ve gerek sanayisi gerek ekonomisi ile bölgenin önemli illerinden biridir Güneydoğu Anadolu,İç Anadolu ve Akdeniz Bölgeleri arasında bir geçit durumundadır.İlin toprakları içinden geçen Seyhan ve Ceyhan Nehri sayesinde çok verimlidir.Adana ili ve il merkezi Türkiye’nin en önemli tarımsal üretim bölgelerinden olan Çukurova Deltası’nda yer alır ve esas olarak gelişimi ve ekonomisi tarımsal üretim ve tarıma dayalı endüstri yoğunlukludur.Adana ilinin genel jeolojik yapısı dağlık kesim ve ovalık kesim olarak iki kesimde incelenir. Jeolojik konumu nedeni ile Adana ili,çok çeşitli ve önemli yer altı kaynaklarına sahiptir.Maden Tetkik ve Arama Genel Müdürlüğünün yaptığı çalışmalar sonucunda önemli metalik maden ve endüstriyel hammadde yatak ve zuhurları ortaya çıkarılmıştır.Bunlar,başta demir olmak üzere krom alüminyum,bakır-kurşun-çinko,manganez,kuvars,kuvarsit,fosfat ve kum-çakıl olarak sayılabilir.",
+    "url": "./adana.html"    
+  },
+  "34": {
+    "title" : "İSTANBUL İLİ MADEN VE ENERJİ KAYNAKLARI",
+    "img" : "./images/istanbul.jpg",
+    "content" : "Türkiye’nin sanayisinin büyük bir kısmının bulunduğu İstanbul ili neredeyse Kocaeli ve Çorlu ile birleşmiş durumdadır. Türkiye’de yapılan ticaretin merkezi konumundadır. İstanbul ili özellikle endüstriyel hammadde kaynakları bakımından önemli potansiyellere sahiptir",
+    "url": "./istanbul.html"    
+  },
+  "06": {
+    "title" : "ANKARA İLİ MADEN VE ENERJİ KAYNAKLARI",
+    "img" : "./images/ankara.jpg",
+    "content" : "Türkiye Cumhuriyeti’nin Başkenti Ankara, Anadolu’nun merkezi bir noktasında kurulmuş olup Kızılırmak ve Sakarya nehirlerinin kollarının oluşturduğu ovalarla kaplı bir ildir.Maden Tetkik Ve Arama Genel Müdürlüğünün Ankara il ve yakın çevresinde yaptığı çalışmalar sonucunda çok sayıda endüstriyel hammadde, metalik maden yatağı ve zuhurları ile jeotermal kaynaklar ortaya çıkarılmıştır. Metalik madenler bakımından ildeki en önemli madenler altın, bakır, kurşun, çinko demir ve manganezdir. Ankara ili kömür oluşumlar ve jeotermal alanlar bakımından da önemli potansiyele sahip olup il sanayisi ve turizmin gelişiminde linyit ve jeotermal kaynaklar belirgin rol oynamaktadır. İlçedeki sıcak su kaynakları şehir ısıtılması ve yerli turizmin gelişiminde ilçe ekonomisine önemli oranlarda katkı sağlamaktadır.",
+    "url": "./ankara.html"    
+  },
+  "35": {
+    "title" : "İZMİR İLİ MADEN VE ENERJİ KAYNAKLARI",
+    "img" : "./images/izmir.jpg",
+    "content" : "İzmir ili kuzeyden Sakarya kıtasının, doğu güneydoğusundan Menderes masifnin, batı-güneybatıdan Karaburun kuşağının çevrelediği İzmir-Ankara zonu üzerinde yer almaktadır. Bölgede ana tektonik hatları kuzeydoğu-güneybatı ve kuzeybatı-güneydoğu yönlü faylar oluşturur. İldeki diğer önemli metalik madenler antimuan, bakır, kurşun ve çinko cevherleşmeleridir.",
+    "url": "./izmir.html"    
+  },
+  "16": {
+    "title" : "BURSA İLİ MADEN VE ENERJİ KAYNAKLARI",
+    "img" : "./images/bursa.jpg",
+    "content" : "Bursa ili Marmara bölgesinin güneyinde yer alır sanayi ve ekonomi bakımından Türkiye’nin önemli illerinden biridir. Bursa ilinin arazisi volkanik bir yapıya sahiptir. Türkiye Deprem Haritasına göre Marmara çevresinde aktif fayların bulunması nedeniyle Bursa 1.Derece deprem kuşağı içinde yer alır. Bursa ili sosyo-ekonomik bakımdan Türkiye’nin en gelişmiş illerindendir.İlin zengin yer altı kaynakları da sanayisinin gelişmesinde etkili olmuştur. ",
+    "url": "./bursa.html"    
+  },
+  "19": {
+    "title" : "Çorum ili Orta Karadeniz bölgesinde yer almaktadır ve Karadeniz Bölgesinin İç Anadolu’ya açılan kapısıdır. Çorum ve yakın çevresinde yapılan çalışmalar sonucunda çok çeşitli metlik ve endüstriyel hammadde yatak ve zuhurları ortaya çıkarılmıştır. Bunların en önemlileri antimuan bakır, demir, manganez, bentonit, grafit, kil kireçtaşı ve tuğla kiremit hammadde yataklarıdır. ",
+    "img" : "./images/çorum.jpg",
+    "content" : "Ülkemizin önemli kuruluşlarının bulunduğu Kocaeli ilinin yüzölçümü oldukça küçüktür. Bu nedenle nüfus yoğunluğunun en fazla olduğu illerin başında gelir. Sanayi kuruluşlarının yoğunluğu ve yatırımların bu ile yapılması nedeniyle İstanbul ili ile tamamen birleşmiş durumdadır.",
+    "url": "./çorum.html"    
+  }
+}
